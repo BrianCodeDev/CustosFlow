ff0e2b779bdc892e8463355895778899a3b88282
Brian Kelley <107327227+BrianCodeDev@users.noreply.github.com>
2024-06-27 13:17:06 -0500

    update 8.0
    Added sidebar curve to my register.tsx file

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # CustosFlow
 CustosFlow is a task monitor dashboard for everyone to get together and use. They can use it for themselves too!
+
+
--- dashboard/src/components/Register.tsx
+++ dashboard/src/components/Register.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import '../App.css'; // Assuming App.css is now located inside src/ directory
 import { MDBBtn, MDBContainer, MDBRow, MDBCol, MDBCard, MDBCardBody, MDBInput, MDBCheckbox, MDBIcon } from 'mdb-react-ui-kit';
 import axios from 'axios';
+import '../App.css';
 
 const App: React.FC = () => {
   const [firstName, setFirstName] = useState('');
@@ -33,35 +34,22 @@ const App: React.FC = () => {
   };
 
   return (
-    <MDBContainer fluid className='register form-content'>
-      <MDBRow>
-        <MDBCol md='6' className='text-center text-md-start d-flex flex-column justify-content-center'>
-          {/* Your existing content */}
-        </MDBCol>
-        <MDBCol md='6'>
-          <MDBCard className=''>
-            <MDBCardBody className='form-padding-one'>
-              {/* Form inputs */}
-              {/* Example: */}
-              <MDBInput wrapperClass='mb-4' label='First name' id='firstName' type='text' onChange={(e) => setFirstName(e.target.value)} />
-              <MDBInput wrapperClass='mb-4' label='Last name' id='lastName' type='text' onChange={(e) => setLastName(e.target.value)} />
-              <MDBInput wrapperClass='mb-4' label='Email' id='email' type='email' onChange={(e) => setEmail(e.target.value)} />
-              <MDBInput wrapperClass='mb-4' label='Password' id='password' type='password' onChange={(e) => setPassword(e.target.value)} />
-              <div className='d-flex justify-content-center mb-4'>
-                <MDBCheckbox name='flexCheck' value='' id='flexCheckDefault' label='Subscribe to our newsletter' onChange={() => setSubscribe(!subscribe)} />
-              </div>
-              {/* Register button */}
-              <MDBBtn className='w-100 mb-4' onClick={handleRegister}>Sign Up</MDBBtn>
-              {/* Social sign-up */}
-              <div className="text-center">
-                <p>or sign up with:</p>
-                {/* Your existing social buttons */}
-              </div>
-            </MDBCardBody>
-          </MDBCard>
-        </MDBCol>
-      </MDBRow>
-    </MDBContainer>
+    <div className="register-body">
+    <div className='display-register'>
+      <div className="sidebar-one">
+      <div className="register-content-sidebar">
+       <p>Welcome to CustosFlow
+       We encourage you to do more with our task tacker system</p>
+       <div className="logo">
+        <img src="https://i.ibb.co/HVrL9hH/Tropical-Leaves-SIlhouettes-14-1-1.png" alt="logo-leaf" />
+       <h1>CustosFlow</h1>
+       </div>
+       <h2>Register to your account</h2>
+       <h3>Don't have an account? <u>Sign Up</u></h3>
+      </div>
+      </div>
+    </div>
+    </div>
   );
 };
 
