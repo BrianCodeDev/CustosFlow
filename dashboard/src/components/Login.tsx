8af88360552d60f94b339f9752a7b2845780a393
Brian Kelley <107327227+BrianCodeDev@users.noreply.github.com>
2024-06-27 17:33:59 -0500

    Update 11.0
    Finished the Register and Login pages in react for front end for now.

--- dashboard/src/components/Login.tsx
+++ dashboard/src/components/Login.tsx
@@ -2,9 +2,10 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import googleicon from '../assets/img/google-icon.svg';
 import githubicon from '../assets/img/github-icon.svg';
+import '../App.css';
 import { Link } from 'react-router-dom';
 
-const Login: React.FC = () => {
+const Register: React.FC = () => {
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
   const [email, setEmail] = useState('');
@@ -42,10 +43,44 @@ const Login: React.FC = () => {
   };
 
   return (
-    <div className="Login-body">
-    <h1>Hello World</h1>
+    <div className="register-body">
+      <div className='display-register'>
+        <div className="sidebar-one">
+          <div className="register-content-sidebar">
+            <p>Welcome to CustoFlow! We encourage you to do more with our task tracker system.</p>
+            <div className="logo">
+              <img src="https://i.ibb.co/HVrL9hH/Tropical-Leaves-SIlhouettes-14-1-1.png" alt="logo-leaf" />
+              <h1>CustosFlow</h1>
+            </div>
+            <h2>Login to your account</h2>
+            <h3>Don't have an account? <Link to="/">Register</Link></h3> {/* Use Link for navigation */}
+            <form onSubmit={handleRegister} className='form-content'>
+              <div className="input-one">
+                <label>Email</label> <br />
+                <input type="text" placeholder='Email here: ' value={email} onChange={(e) => setEmail(e.target.value)} />
+              </div>
+              <div className="input-two">
+                <label>Password</label> <br />
+                <input type="password" placeholder='Password here: ' value={password} onChange={(e) => setPassword(e.target.value)} />
+              </div>
+              <button type="submit">Login</button>
+            </form>
+            <div className="display-social-button">
+              <div className="google-button">
+                <button><img src={googleicon} alt="google-icon" />Google</button>
+              </div>
+              <div className="github-button">
+                <button><img src={githubicon} alt="github-icon" />Github</button>
+              </div>
+            </div>
+            <div className="read-more">
+              <a href="#">Read More Here</a>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
 
-export default Login;
+export default Register;
